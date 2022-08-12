e5d0d61fc4d3d7d3fcf26fdac5420bd234117997
altsys <info@altsys.es>
2022-08-12 21:11:16 +0200

    Se añade archivo setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='audittor',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='altsys',
+    author_email='info@altsys.es',
+    description=''
+)
