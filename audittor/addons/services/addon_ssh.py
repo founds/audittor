901d6285edb2647a0598effa3b8476c10aa8642f
altsys <info@altsys.es>
2022-09-16 23:15:30 +0200

    Se solventa la llamada a la consultas en el modulo ssh

--- audittor/addons/services/addon_ssh.py
+++ audittor/addons/services/addon_ssh.py
@@ -32,16 +32,15 @@ def check_ssh():
         print(f"{GREEN}     - No se ha encontrado servicio SSH activo.{RESET}")
         return True
 
-def checks(datas):
-    for data in datas:
-        if "Port 22" in data:
-            print(f"{RED}   - Se está usando el puerto 22, este es un puerto por defecto.{RESET}")
+def checks(data):
+    if "Port 22" in data:
+        print(f"{RED}   - Se está usando el puerto 22, este es un puerto por defecto.{RESET}")
 
-        if "PermitRootLogin yes" or "#PermitRootLogin no" in data:
-            print(f"{RED}   - Se permite el acceso al usuario root.{RESET}")
+    if "PermitRootLogin yes" or "#PermitRootLogin no" in data:
+        print(f"{RED}   - Se permite el acceso al usuario root.{RESET}")
 
-        if "#StrictModes yes" or "StrictModes no" in data:
-            print(f"{RED}   - El modo estricto no esta activo.{RESET}")
+    if "#StrictModes yes" or "StrictModes no" in data:
+        print(f"{RED}   - El modo estricto no esta activo.{RESET}")
 
 
 
