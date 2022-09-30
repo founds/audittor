fd6b9024ec999de987aedac02b863fb7ee3b94dc
altsys <info@altsys.es>
2022-09-30 23:51:42 +0200

    Se añade opciones al modulo ssh

--- audittor/addons/services/addon_ssh.py
+++ audittor/addons/services/addon_ssh.py
@@ -37,62 +37,67 @@ def checks(data):
     nerrors = 0
 
     if "Port 22" in data:
-        print(f"{RED}       - Se está usando el puerto 22, este es un puerto por defecto.{RESET}")
+        print(f"{RED}       - Port: Se está usando el puerto 22, este es un puerto por defecto.{RESET}")
         print("         No se recomienda usar el puerto por defecto.\n")
         nerrors += 1
 
     if "PermitRootLogin yes" or "#PermitRootLogin no" in data:
-        print(f"{RED}       - Se permite el acceso al usuario root.{RESET}")
+        print(f"{RED}       - PermitRootLogin: Se permite el acceso al usuario root.{RESET}")
         print("         .\n")
         nerrors += 1
 
     if "#StrictModes yes" in data:
-        print(f"{RED}       - El modo estricto no esta activo.{RESET}")
+        print(f"{RED}       - StrictModes: El modo estricto no esta activo.{RESET}")
         print("         .\n")
         nerrors += 1
 
     if "StrictModes no" in data:
-        print(f"{RED}       - El modo estricto no esta activo.{RESET}")
+        print(f"{RED}       - StrictModes: El modo estricto no esta activo.{RESET}")
         print("         .\n")
         nerrors += 1
 
-    if "#bantime" in data:
-        print(f"{RED}       - No esta establecido el tiempo de baneo.{RESET}")
+    if "#Bantime" in data:
+        print(f"{RED}       - Bantime: No esta establecido el tiempo de baneo.{RESET}")
         print("         .\n")
         nerrors += 1
 
     if "#maxretry" in data:
-        print(f"{RED}       - No esta establecido el número de maximo de intentos.{RESET}")
+        print(f"{RED}       - maxretry: No esta establecido el número de maximo de intentos.{RESET}")
         print("         Tiempo para la desconeción si el usuario no logra logarse.\n")
         nerrors += 1
 
     if "#Protocol" in data:
-        print(f"{RED}       - No esta establecido el número de maximo de intentos.{RESET}")
-        print("         Tiempo para la desconeción si el usuario no logra logarse.\n")
+        print(f"{RED}       - Protocol: No esta establecido el protocolo de conexión.{RESET}")
+        print("         .\n")
         nerrors += 1
 
     if "Protocol" not in data:
-        print(f"{RED}       - No esta establecido el número de maximo de intentos.{RESET}")
+        print(f"{RED}       - Protocol: No esta establecido el número de maximo de intentos.{RESET}")
         print("         Tiempo para la desconeción si el usuario no logra logarse.\n")
         nerrors += 1
 
     if "#LoginGraceTime" in data:
-        print(f"{RED}       - No esta establecido el tiempo maximo de loging.{RESET}")
+        print(f"{RED}       - LoginGraceTime: No esta establecido el tiempo maximo de loging.{RESET}")
+        print("         Tiempo que tiene un usuario para loguearse en el sistema correctamente.\n")
+        nerrors += 1
+    if "LoginGraceTime" not in data:
+        print(f"{RED}       - LoginGraceTime: No esta establecido el tiempo maximo de loging.{RESET}")
         print("         Tiempo que tiene un usuario para loguearse en el sistema correctamente.\n")
         nerrors += 1
 
     if "#UsePrivilegeSeparation" in data:
-        print(f"{RED}       - No esta establecido la separación de privilegios.{RESET}")
+        print(f"{RED}       - UsePrivilegeSeparation: No esta establecido la separación de privilegios.{RESET}")
         print("         .\n")
         nerrors += 1
 
     if "#MaxAuthTries" in data:
-        print(f"{RED}       - No esta especificado el número de intentos de autenticación permitidos por conexión.{RESET}")
+        print(f"{RED}       - MaxAuthTries: No esta especificado el número de intentos de autenticación permitidos "
+              f"por conexión.{RESET}")
         print("         Número máximo de intentos de autenticación permitidos por conexión.\n")
         nerrors += 1
 
     if "#MaxSessions" in data:
-        print(f"{RED}       - No esta especificado el número máximo de sessiones.{RESET}")
+        print(f"{RED}       - MaxSessions: No esta especificado el número máximo de sessiones.{RESET}")
         print("         Permite especificar el número máximo de sesiones abiertas.\n")
         nerrors += 1
 
